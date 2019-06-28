805de9c84cc5c925467b057a767b1e574b85ff63
An Huynh <ahuynh@emailatg.com>
2019-06-27 19:33:40 -0500

    text in arc path

--- zsbc_local/app.js
+++ zsbc_local/app.js
@@ -1,4 +1,4 @@
-var root = {
+var p_score = {
 	"name": "TOTAL",
 	"color": "#FFF",
 	"children": [
@@ -124,46 +124,23 @@ var root = {
 	]
 }
 
-// set width, height, and radius
 var width = 425,
 	height = 425,
-	radius = (Math.min(width, height) / 2) - 10; // lowest number divided by 2. Then subtract 10
+	radius = (Math.min(width, height)/2);
 
-var x = d3.scaleLinear() // continuous scale. preserves proportional differences 
-	.range([0, 2 * Math.PI]); // setting range from 0 to 2 * circumference of a circle 
+var x = d3.scaleLinear().range([0, 2 * Math.PI]);
 
-var y = d3.scaleSqrt() // continuous power scale 
-	.range([0, radius]); // setting range from 0 to radius 
+var y = d3.scaleSqrt().range([0, radius]);
 
-var y1 = d3.scaleLinear()
-	.range([0, radius]);
+var partition = d3.partition();
 
-
-var partition = d3.partition(); // subdivides layers 
-
-// define arcs
 var arc = d3.arc()
-	.startAngle(function (d) { return Math.max(0, Math.min(2 * Math.PI, x(d.x0))); })
-	.endAngle(function (d) { return Math.max(0, Math.min(2 * Math.PI, x(d.x1))); })
-	.innerRadius(function (d) { return Math.max(0, y(d.y0)); })
-	.outerRadius(function (d) { return Math.max(0, y(d.y1)); });
+	.startAngle((d) => {return Math.max(0, Math.min(2 * Math.PI, x(d.x0))); })
+	.endAngle((d) => {return Math.max(0, Math.min(2 * Math.PI, x(d.x1))); })
+	.innerRadius((d) =>{return Math.max(0, y(d.y0)); })
+	.outerRadius((d) =>{return Math.max(0, y(d.y1)); });
 
-// define tooltip
-var tooltip = d3.select('body') // select element in the DOM with id 'chart'
-	.append('div').classed('tooltip', true); // append a div element to the element we've selected
-tooltip.append('div') // add divs to the tooltip defined above
-	.attr('class', 'label'); // add class 'label' on the selection
-tooltip.append('div') // add divs to the tooltip defined above
-	.attr('class', 'count'); // add class 'count' on the selection
-tooltip.append('div') // add divs to the tooltip defined above
-	.attr('class', 'percent'); // add class 'percent' on the selection
-
-// prep the data
-var root = d3.hierarchy(root);
-
-// calculate total
-var total = 100;
-
+var root = d3.hierarchy(p_score);
 
 root.sum(function (d) {
 	if (typeof d.size !== 'undefined'){
@@ -171,128 +148,102 @@ root.sum(function (d) {
 	}
 	return d.size; 
 });
+ 
 
-// define SVG element
-const svg = d3.select("#chart").append("svg")
-	.attr("width", width) // set width
-	.attr("height", height) // set height
-	.append("g") // append g element
+root.data.children.forEach((d) => {	d.enabled = true;});
+
+var svg = d3.select('.chart').append("svg")
+	.attr("width", width)
+	.attr("height", height)
+	.append("g")
+	.attr("id", "wrapper")
 	.attr("transform", "translate(" + width / 2 + "," + (height / 2) + ")");
 
-// redraw(root);
-//technically group
-const group = svg.selectAll("g")
-	.data(partition(root).descendants()) // path for each descendant
+
+var arcGroup = svg.selectAll('.wrapper')
+	.data(partition(root).descendants())
 	.enter()
-		.append("g")
-		.attr("class", "shape");
+	.append("g")
+	.attr("class", "arc_group");
 
-group.append("title")
-.text(function (d) { return d.data.name });
+var svg_arc = arcGroup.append("path")
+	.attr("d", arc) // draw arcs
+	.attr("class", "arc")
+	.attr("id", (d) => {return d.data.name + "_arc"})
+	.style("fill",  (d) => {return (d.children ? d : d.parent).data.color;})
+	.on("click", click);
 
+// var hidden_arc = arcGroup.append("path")
+// 	.attr("class", "hidden_arc")
+// 	.attr("id", (d) => {return d.data.name + "_hidden_arc"})
+// 	.style("fill", "none")
+// 	.on("click", click)
+// 	.each((d, i) => {
+// 		var firstArc = RegExp("/(^.+?)L/","g");
+// 		var newArc =  firstArc.exec( d3.select(this).attr("d"));
+// 		if (d.endAngle > 90 * Math.PI/180) {
+// 			var startLoc 	= RegExp('/M(.*?)A/'),
+// 				middleLoc 	= RegExp('/A(.*?)0 0 1/'),
+// 				endLoc 		= RegExp('/0 0 1 (.*?)$/');
+// 			var newStart = endLoc.exec( newArc )[1];
+// 			var newEnd = startLoc.exec( newArc )[1];
+// 			var middleSec = middleLoc.exec( newArc )[1];
+// 			newArc = "M" + newStart + "A" + middleSec + "0 0 0 hello" + newEnd;
+// 	}
 
-//	adds tool tip on mouse over
-	group.on('mouseover', function (d) {
-		var percent = d.value / total; // calculate percent
-		tooltip.select('.label').html("<h4>" + d.data.name + "<h4>"); // set current label           
-		tooltip.select('.count').html("Percent: " + d.value.toFixed(4) + "%"); // set current count            
-		tooltip.select('.percent').html("Weight: " + percent.toFixed(4)); // set percent calculated above          
-		tooltip.style('display', 'block'); // set display
-	})
-	.on('mouseout', function () { // when mouse leaves div                        
-		tooltip.style('display', 'none'); // hide tooltip for that element
-	})
-	.on('mousemove', function (d) { // when mouse moves                  
-		tooltip.style('top', (d3.event.layerY + 10) + 'px'); // always 10px below the cursor
-		tooltip.style('left', (d3.event.layerX + 10) + 'px'); // always 10px to the right of the mouse
-	});
-
-var path = group.append("path")
-		.attr("d", arc) // draw arcs
-		.attr("class", "path")
-		.style("fill", function (d) { 
-			return (d.children ? d : d.parent).data.color; 
-		})
-		.on("click", click);
-//d3.select(self.frameElement).style("height", height + "px");
-
-// var text = group.append("text")
-// .text(function (d) { 
-// 	return d.data.name 
-// })
-// .attr("x", function(d, i) {
-// 		return (d.children ? d.children[i] : d);
-// })
-// .attr("y", function(d, i) {
-// 	return y((d.children ? d.children[i] : d).y0);
+// 	arcGroup.append("path")
+//         .attr("class", "hidden_arc")
+//         .attr("id", "hidden_arc"+i)
+//         .attr("d", newArc)
+//         .style("fill", "none");
 // });
 
-var text = group.append("text")
-	.attr("transform", function(d) { return "rotate(" + computeTextRotation(d) + ")"; })
-	.attr("x", 
-		function(d, i) { 
-			return y1(d.y1); 
-		})
-		//.attr("y", function(d) { return x(d.y0); })
-		// .attr("y", 
-		// function(d, i) { 
-		// 	return y(d.x1); 
-		// })
-	.attr("dx", "6") // margin
-	.attr("dy", ".35em") // vertical-align
-	.text(function(d) { return d.data.name; });
+
+var title = arcGroup.append("title")
+.text((d) => { return d.parent !== 'null' ? d.data.name : "" });
+
+var text = arcGroup
+	.append("text")
+	.attr("x", 10)
+	.attr("dy", 20)
+	//.attr("dy", (d,i) => { return (d.endAngle > 90 * Math.PI/180 ? 18 : -11); })
+	.append("textPath")
+	//.attr('y', '2em')
+	//.attr("startOffset","50%")
+	//.style("text-anchor","middle")
+	.attr("xlink:href", (d) => {return "#" + d.data.name + "_arc"})
+	.text((d) => { 
+		console.log(d.data.name, d.depth)
+		return d.parent !== 'null' ? d.data.name : "" 
+	});
 
 
-//appends the total in the middle of the chart after the chart has been drawn. 
-svg.append("text")
-	.attr("class", "total")
-	.attr("text-anchor", "middle")
-	.attr('font-size', '4em')
-	.attr('y', 20)
-	.text(parseFloat(total.toFixed(2)) + "%");
+//////////////////////////////////////////////////////////////////////////////
+///////////////////////////functions/////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////
 
-// redraw on disabled category
-function redraw(d) {
-	console.log("function redraw");
 
-	svg.transition()
-		.duration(750)
-		.tween("scale", function () {
-			var xd = d3.interpolate(x.domain(), [d.x0, d.x1]),
-				yd = d3.interpolate(y.domain(), [d.y0, 1]),
-				yr = d3.interpolate(y.range(), [d.y0 ? (radius / 2) : 0, radius]);
-			return function (t) { x.domain(xd(t)); y.domain(yd(t)).range(yr(t)); };
-		})
-		.selectAll("g")
-		.attrTween("d", function (d) { return function () { return arc(d); }; });
-
-	//d3.select(".total").text(d.value.toFixed(0) + "%");
-}
-
-// zoom on click
 function click(d) {
-	console.log("function click");
-	console.log("d.y0 = " + d.y0);
-
 	svg.transition()
 		.duration(650) // duration of transition
-		.tween("scale", function () {
+		.tween("scale", () => {
 			var xd = d3.interpolate(x.domain(), [d.x0, d.x1]),
 				yd = d3.interpolate(y.domain(), [d.y0, 1]),
 				yr = d3.interpolate(y.range(), [d.y0 ? (80) : 0, radius]);
-			return function (t) { x.domain(xd(t)); y.domain(yd(t)).range(yr(t)); };
+			return (t) => { x.domain(xd(t)); y.domain(yd(t)).range(yr(t)); };
 		})
 		.selectAll("path")
-		.attrTween("d", function (d) { return function () { return arc(d); }; });
-		//making this round up to 100% for just showing purposes.
+		.attrTween("d", (d) => { 
+			return () => { return arc(d); }; });
 	d3.select(".total").text(d.value.toFixed(0) === "100" ? "100%" : d.value.toFixed(2) + "%");
 }
 
 
-function computeTextRotation(d) {
-	var angle = (d.x0 + d.x1) / Math.PI * 90;
-	// Avoid upside-down labels
-	return (angle < 120 || angle > 270) ? angle : angle + 180;  // labels as rims
-	//return (angle < 180) ? angle - 90 : angle + 90;  // labels as spokes
-}
-
+// middle total
+var total = 100;
+svg.append("text")
+.attr("class", "total")
+.attr("text-anchor", "middle")
+.attr('font-size', '4em')
+.attr('y', 20)
+.text(total + "%");
